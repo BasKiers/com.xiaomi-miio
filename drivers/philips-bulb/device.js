d4edc9570a2ad6cccc7467400e5fad7560ec4341
jghaanstra <jghaanstra@gmail.com>
2018-05-28 18:43:29 +0200

    Debug philips lights

--- drivers/philips-bulb/device.js
+++ drivers/philips-bulb/device.js
@@ -6,89 +6,95 @@ const miio = require('miio');
 
 class PhilipsBulbDevice extends Homey.Device {
 
-    onInit() {
-        this.createDevice();
+  onInit() {
+    this.createDevice();
 
-        this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
-        this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this));
-        this.registerCapabilityListener('light_temperature', this.onCapabilityLightTemperature.bind(this));
-    }
+    console.log("Initializing device");
 
-    onDeleted() {
-        clearInterval(this.pollingInterval);
-        this.miio.destroy();
-    }
+    this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+    this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this));
+    this.registerCapabilityListener('light_temperature', this.onCapabilityLightTemperature.bind(this));
+  }
 
-    // LISTENERS FOR UPDATING CAPABILITIES
-    onCapabilityOnoff(value, opts, callback) {
-        this.miio.setPower(value)
-            .then(result => { callback(null, value) })
-            .catch(error => { callback(error, false) });
-    }
+  onDeleted() {
+    clearInterval(this.pollingInterval);
+    this.miio.destroy();
+  }
 
-    onCapabilityDim(value, opts, callback) {
-        var brightness = value * 100;
-        this.miio.setBrightness(brightness)
-            .then(result => { callback(null, value) })
-            .catch(error => { callback(error, false) });
-    }
+  // LISTENERS FOR UPDATING CAPABILITIES
+  onCapabilityOnoff(value, opts, callback) {
+    this.miio.setPower(value)
+      .then(result => { callback(null, value) })
+      .catch(error => { callback(error, false) });
+  }
 
-    onCapabilityLightTemperature(value, opts, callback) {
-        var colorvalue = util.denormalize(value, 3000, 5700);
-        var colortemp = ''+ colorvalue +'K';
-        this.miio.color(colortemp)
-            .then(result => { callback(null, value) })
-            .catch(error => { callback(error, false) });
-    }
+  onCapabilityDim(value, opts, callback) {
+    var brightness = value * 100;
+    this.miio.setBrightness(brightness)
+      .then(result => { callback(null, value) })
+      .catch(error => { callback(error, false) });
+  }
 
-    // HELPER FUNCTIONS
-    createDevice() {
-        miio.device({
-                address: this.getSetting('address'),
-                token: this.getSetting('token')
-            }).then(miiodevice => {
-                this.miio = miiodevice;
+  onCapabilityLightTemperature(value, opts, callback) {
+    var colorvalue = util.denormalize(value, 3000, 5700);
+    var colortemp = ''+ colorvalue +'K';
+    this.miio.color(colortemp)
+      .then(result => { callback(null, value) })
+      .catch(error => { callback(error, false) });
+  }
 
-                var interval = this.getSetting('polling') || 60;
-                this.pollDevice(interval);
-        }).catch(function (error) {
-            return reject(error);
-        });
-    }
+  // HELPER FUNCTIONS
+  createDevice() {
+    console.log("Creating device");
+    miio.device({
+        address: this.getSetting('address'),
+        token: this.getSetting('token')
+      }).then(miiodevice => {
+        this.miio = miiodevice;
 
-    pollDevice(interval) {
-        clearInterval(this.pollingInterval);
+        var interval = this.getSetting('polling') || 60;
+        this.pollDevice(interval);
+    }).catch(function (error) {
+      return reject(error);
+      console.log("Error:");
+      console.log(error);
+    });
+  }
 
-        this.pollingInterval = setInterval(() => {
-            const getData = async () => {
-                try {
-                    const power = await this.miio.power();
-                    const brightness = await this.miio.brightness()
-                    const colorTemperature = await this.miio.color();
+  pollDevice(interval) {
+    clearInterval(this.pollingInterval);
+    console.log("Polling device");
 
-                    if (this.getCapabilityValue('onoff') != power) {
-                        this.setCapabilityValue('onoff', power);
-                    }
-                    var dim = brightness / 100;
-                    if (this.getCapabilityValue('dim') != dim) {
-                        this.setCapabilityValue('dim', dim);
-                    }
-                    var colorvalue = colorTemperature.replace('K', '');
-                    var colortemp = util.normalize(colorvalue, 3000, 5700);
-                    if (this.getCapabilityValue('light_temperature') != colortemp) {
-                        this.setCapabilityValue('light_temperature', colortemp);
-                    }
-                    if (!this.getAvailable()) {
-                        this.setAvailable();
-                    }
-                } catch (error) {
-                    this.setUnavailable(Homey.__('unreachable'));
-                    this.log(error);
-                }
-            }
-            getData();
-        }, 1000 * interval);
-    }
+    this.pollingInterval = setInterval(() => {
+      const getData = async () => {
+        try {
+          const power = await this.miio.power();
+          const brightness = await this.miio.brightness()
+          const colorTemperature = await this.miio.color();
+
+          if (this.getCapabilityValue('onoff') != power) {
+            this.setCapabilityValue('onoff', power);
+          }
+          var dim = brightness / 100;
+          if (this.getCapabilityValue('dim') != dim) {
+            this.setCapabilityValue('dim', dim);
+          }
+          var colorvalue = colorTemperature.replace('K', '');
+          var colortemp = util.normalize(colorvalue, 3000, 5700);
+          if (this.getCapabilityValue('light_temperature') != colortemp) {
+            this.setCapabilityValue('light_temperature', colortemp);
+          }
+          if (!this.getAvailable()) {
+            this.setAvailable();
+          }
+        } catch (error) {
+          this.setUnavailable(Homey.__('unreachable'));
+          this.log(error);
+        }
+      }
+      getData();
+    }, 1000 * interval);
+  }
 
 }
 
